d7b595173e7f86554da6d8d7d2b0c039789a0eaa
OlesjaD <olesja.d.c@gmail.com>
2023-03-06 16:32:25 +0200

    markup 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+const createGaleryItems = createGaleryEl(galleryItems);
+containerGallery.insertAdjacentHTML("beforeend", createGaleryItems);
+
+containerGallery.addEventListener('click', onGalleryClick);
+
+function createGaleryEl(galleryItems) {
+    return galleryItems 
+        .map(({preview, original, description}) => {
+            return `
+                    <a class="gallery__item" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            alt="${description}"
+                        />
+                    </a>
+            `;
+        }).join("");
+};
+function onGalleryClick (event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
+
+};
