b1b24612e32e0db3e64b8e0b7b512e6d96fe901f
OlesjaD <olesja.d.c@gmail.com>
2023-03-10 00:36:36 +0200

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const idPlayer = new player('vimeo-player');
+
+const onPlay = function(currentTime) {
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+    const timeVideoPause = currentTime.seconds;
+    // console.log('time:', timeVideoPause);
+    localStorage.setItem("videoplayer-current-time", timeVideoPause);
+};
+
+idPlayer.on('pause', throttle(onPlay, 1000));
+
+savedTime = localStorage.getItem("videoplayer-current-time");
+idPlayer.setCurrentTime(savedTime).then(function() {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
